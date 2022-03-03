3fcf1a31a8370814818dfabb49c8315c41df9711
aeiou80 <cameronjdavis@cmail.carleton.ca>
2022-03-03 13:22:29 -0500

    add tests using mockMvc

---
+++ src/test/java/com/example/testingweb/TestingWebApplicationTest.java
@@ -0,0 +1,37 @@
+package com.example.testingweb;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestingWebApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnNewAddressBook() throws Exception {
+        this.mockMvc.perform(post("/addressbook")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"buddyInfos\":[],\"size\":0}")));
+    }
+
+    @Test
+    public void shouldReturnAddressBook() throws Exception {
+        this.mockMvc.perform(post("/addressbook")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/addressbook/1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"id\":1,\"buddyInfos\":[],\"size\":0}")));
+    }
+
+}
